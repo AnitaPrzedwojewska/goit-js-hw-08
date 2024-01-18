66b53fdce5edfddab58f1c2d5ef1aa66b7dc0373
AnitaPrzedwojewska <nisiek@wp.pl>
2024-01-18 21:57:30 +0100

    Corrected throttle in task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,43 +3,33 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-form-state';
 
-const formState = { email: '', message: '' };
+const formState = { email: '', message: ''};
 
 try {
   formState.email = JSON.parse(localStorage.getItem(localStorageKey)).email;
 } catch {
-  // nothing
+  // nothing to do
 } finally {
   form.elements.email.value = formState.email;
 }
 try {
   formState.message = JSON.parse(localStorage.getItem(localStorageKey)).message;
 } catch {
-  // nothing
+  // nothing to do
 } finally {
   form.elements.message.value = formState.message;
 }
 
-form.elements.email.addEventListener('input', event => {
+form.elements.email.addEventListener('input', throttle((event) => {
   formState.email = event.target.value;
-  throttle(
-    localStorage.setItem(localStorageKey, JSON.stringify(formState)),
-    500
-  );
-  console.log(formState);
-  console.log(currentTime);
-});
-form.elements.message.addEventListener('input', event => {
+  localStorage.setItem(localStorageKey, JSON.stringify(formState));
+}, 500));
+form.elements.message.addEventListener('input', throttle((event) => {
   formState.message = event.target.value;
-  throttle(
-    localStorage.setItem(localStorageKey, JSON.stringify(formState)),
-    500
-  );
-  console.log(formState);
-  console.log(currentTime);
-});
+  localStorage.setItem(localStorageKey, JSON.stringify(formState));
+}, 500));
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log(formState);
   localStorage.removeItem(localStorageKey);
